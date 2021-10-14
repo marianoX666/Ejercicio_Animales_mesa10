e0f6615aa4c00fde974dbc77bd2bf3fca2f1229e
Victor Schweizer <walterio12345@gmail.com>
2021-10-14 12:00:10 -0300

    Mensaje de main

--- Animalitos mesa10/src/animalitos/mesa10/AnimalitosMesa10.java
+++ Animalitos mesa10/src/animalitos/mesa10/AnimalitosMesa10.java
@@ -7,7 +7,7 @@ public class AnimalitosMesa10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Esta es la Mejor mesa de EGG");
         
     }
     
